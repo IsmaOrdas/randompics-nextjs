a10279ed1fb0e37290e75c595d81a65e8d386c46
Ismael Ordás <ismael.ordas@mimacom.com>
2021-05-04 16:17:34 +0200

    Remove utils file

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../../styles/AppLayout.module.scss';
 import Header from '../Header'
-import useDebounce from '../../utils/index';
 
 export default function AppLayout({children}) {
-
     const [scrollTop, setScrollTop] = useState(0); 
 
     const handleScroll = () => {
         setTimeout(() => {
             setScrollTop(window.scrollY)
-            console.log(scrollTop, window.scrollY);
         }, 250);
     }
     useEffect(() => {
