6ee1d58ec928715fe0852ac38acb3b7b7c0828b5
Ismael Ordás <ismael.ordas@mimacom.com>
2021-05-04 14:18:26 +0200

    Start project. App layout and basic styles.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+import AppLayout from '../componentes/AppLayout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  )
 }
 
 export default MyApp
