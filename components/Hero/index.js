ed8d24566e5b607ce7b6aab9443b6d0f029652c6
Ismael Ordás <ismael.ordas@mimacom.com>
2021-05-06 17:11:16 +0200

    Fix Hero

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -5,7 +5,7 @@ export default function Hero(props) {
     return (
         <div className={styles.hero}>
             <div className={styles["hero--left"]}>
-                <h2>Are you bored?</h2>
+                <h2 className={styles["hero__title"]}>Are you bored?</h2>
                 <p>CoolPics helps you to spend hours of your day scrolling down and sharing a big list of random images. </p>
                 <Button type="primary">Start here</Button>
             </div>
