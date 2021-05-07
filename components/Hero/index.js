c204e5afc1983e8acf1bb53ec9e4a02086429c96
Ismael Ordás <ismael.ordas@mimacom.com>
2021-05-07 12:32:30 +0200

    Rem function. Improve layout

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -4,12 +4,14 @@ import Button from '../Button';
 export default function Hero(props) {
     return (
         <div className={styles.hero}>
-            <div className={styles["hero--left"]}>
-                <h2 className={styles["hero__title"]}>Are you bored?</h2>
-                <p>CoolPics helps you to spend hours of your day scrolling down and sharing a big list of random images. </p>
-                <Button type="primary">Start here</Button>
+            <div className={styles["hero__wrap"]}>
+                <div className={styles["hero--left"]}>
+                    <h2 className={styles["hero__title"]}>Are you bored?</h2>
+                    <p>CoolPics helps you to spend hours of your day scrolling down and sharing a big list of random images. </p>
+                    <Button type="primary">Start here</Button>
+                </div>
+                <div className={styles["hero--right"]}>{props.right}</div>
             </div>
-            <div className={styles["hero--right"]}>{props.right}</div>
         </div>
     )
 }
